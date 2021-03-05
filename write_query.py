ce7a48bb42fc24ca9ef0116369dedb7c9a687e05
Jake Barrett <jake.barrett@ntlworld.com>
2021-03-05 17:12:04 +0000

    Add pub_id_query.txt

--- write_query.py
+++ write_query.py
@@ -1,6 +1,8 @@
 from hdruk_groups import *
 
-def write_query(workbook):
+PATH_TO_DIMENSIONS_2019_EXPORT = "dimensions/ALL_Dimensions-Publication-2021-03-04_11-51-46.xlsx"
+
+def write_doi_query(workbook):
 
     publications_df = workbook['Publications']
 
@@ -22,13 +24,39 @@ def write_query(workbook):
     doi_file.close()
 
 
+def write_publication_id_query(workbook):
+
+    dimensions_df = workbook['Sheet1']
+
+    pub_id_column = dimensions_df['Publication ID'].dropna()
+
+    pub_id_unique = pub_id_column.drop_duplicates()
+
+    OR_query_pub_id = 'id: ("'
+    count=0
+    for i in pub_id_unique:
+        if count == 0:
+            OR_query_pub_id = OR_query_pub_id + i
+        else:
+            OR_query_pub_id = OR_query_pub_id + '" OR "' + i
+        count+=1
+
+    OR_query_pub_id = OR_query_pub_id + '")'
+
+    pub_id_file = open("dimensions/pub_id_query.txt", "wt")
+    write_ids = pub_id_file.write(OR_query_pub_id)
+    pub_id_file.close()
+
+
 def main():
 
     rf_2018_wb = read_workbook(PATH_TO_RESEARCHFISH_2018_DATA)
     rf_2019_wb = read_workbook(PATH_TO_RESEARCHFISH_2019_DATA)
 
-    write_query(rf_2019_wb)
+    dimensions_export = read_workbook(PATH_TO_DIMENSIONS_2019_EXPORT)
 
+    # write_doi_query(rf_2019_wb)
+    write_publication_id_query(dimensions_export)
 
 if '__main__' == __name__:
     main()
