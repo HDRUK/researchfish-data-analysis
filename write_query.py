8d2bd19140b9f8ba53c774eed08a561e8b61dfa8
Jake Barrett <jake.barrett@ntlworld.com>
2021-03-08 22:12:48 +0000

    Add Dimensions extract for RFish 2018 data

--- write_query.py
+++ write_query.py
@@ -1,8 +1,9 @@
 from hdruk_groups import *
 
+PATH_TO_DIMENSIONS_2018_EXPORT = "dimensions/Dimensions_2018.xlsx"
 PATH_TO_DIMENSIONS_2019_EXPORT = "dimensions/ALL_Dimensions-Publication-2021-03-04_11-51-46.xlsx"
 
-def write_doi_query(workbook):
+def write_doi_query(workbook, year):
 
     publications_df = workbook['Publications']
 
@@ -19,12 +20,12 @@ def write_doi_query(workbook):
             OR_query_doi = OR_query_doi + ' OR ' + doi
         count+=1
 
-    doi_file = open("dimensions/doi_query.txt", "wt")
+    doi_file = open("dimensions/doi_query_{}.txt".format(year), "wt")
     write_dois = doi_file.write(OR_query_doi)
     doi_file.close()
 
 
-def write_publication_id_query(workbook):
+def write_publication_id_query(workbook, year):
 
     dimensions_df = workbook['Sheet1']
 
@@ -43,7 +44,7 @@ def write_publication_id_query(workbook):
 
     OR_query_pub_id = OR_query_pub_id + '")'
 
-    pub_id_file = open("dimensions/pub_id_query.txt", "wt")
+    pub_id_file = open("dimensions/pub_id_query_{}.txt".format(year), "wt")
     write_ids = pub_id_file.write(OR_query_pub_id)
     pub_id_file.close()
 
@@ -53,10 +54,17 @@ def main():
     rf_2018_wb = read_workbook(PATH_TO_RESEARCHFISH_2018_DATA)
     rf_2019_wb = read_workbook(PATH_TO_RESEARCHFISH_2019_DATA)
 
-    dimensions_export = read_workbook(PATH_TO_DIMENSIONS_2019_EXPORT)
+    dimensions_2018_export = read_workbook(PATH_TO_DIMENSIONS_2018_EXPORT)
+    dimensions_2019_export = read_workbook(PATH_TO_DIMENSIONS_2019_EXPORT)
 
-    # write_doi_query(rf_2019_wb)
-    write_publication_id_query(dimensions_export)
+
+    write_doi_query(rf_2018_wb, "2018")
+    write_doi_query(rf_2019_wb, "2019")
+    write_publication_id_query(dimensions_2018_export, "2018")
+    write_publication_id_query(dimensions_2019_export, "2019")
+
+    
+
 
 if '__main__' == __name__:
     main()
